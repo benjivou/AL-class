54c6c46cb594a575184f7fc12181fb1a87746579
raniafe <rania.fekih@etu.unice.fr>
2020-10-14 23:51:15 +0200

    Link MongoDB with Backend #1

--- ticket-check/app/api/index.js
+++ ticket-check/app/api/index.js
@@ -1,12 +1,45 @@
-var express  = require('express');
-var  bodyParser = require('body-parser');
-var app =  express();
+const express  = require('express');
+const bodyParser = require('body-parser');
+const app =  express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+const Ticket = require('../models/infos');
 
-app.use(bodyParser.json())
+const currentTrainId ="ABCDEFYIS";
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+app.get("/:id", async (req, res) => {
+    try{
+        const ticket = await Ticket.findById(req.params.id);
+        if(ticket.trainRef === currentTrainId){
+            await res.json(true);
+        }else {
+            await res.json(false);
+        }
+
+    }catch(err) {
+        await res.json({message: err});
+    }
+});
+
+app.post("/",async (req,res) => {
+    const ticket = new Ticket({
+        _id : req.body.id ,
+        passengerName : req.body.passengerName ,
+        type : req.body.type ,
+        trainRef: req.body.trainRef
+    });
+    try{
+        const savedTicket = await ticket.save();
+        await res.json(savedTicket);
+    }catch(err) {
+        await res.json({message: err});
+    }
+});
 
 
 
--- ticket-check/index.js
+++ ticket-check/index.js
@@ -1,44 +1,46 @@
-
-
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const morgan = require('morgan')
-app.use(cors())
-const bodyParser = require('body-parser')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan');
+app.use(cors());
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     next()
 });
 
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(morgan('short'))
-
-
+app.use(morgan('short'));
 
 
 
-const ticketCheckService_router = require('./app/api/index.js')
+mongoose.connect('mongodb+srv://user:al123@cluster0.dsrqm.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(()=>{
+    console.log(`connection to database established`)});
 
-app.use('/ticketCheck',ticketCheckService_router)
+const ticketCheckService_router = require('./app/api/index.js');
+
+app.use('/ticketCheck',ticketCheckService_router);
 
 
 
-
-app.get("/", (req, res) => {
-    console.log("Responding to root route")
-    res.send("Hello from ROOOOOT")
-})
 
 
 // localhost:3003
 app.listen(3003, () => {
     console.log(" Ticket ckeck Server is up and listening on 3003...")
-})
+});
