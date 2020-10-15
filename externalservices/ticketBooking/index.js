620c44e8d18c3b5cc8f9585450f38c422f89608c
raniafe <rania.fekih@etu.unice.fr>
2020-10-15 21:26:37 +0200

    ticket booking external service #2

---
+++ externalservices/ticketBooking/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan');
+app.use(cors());
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    next()
+});
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(morgan('short'));
+
+
+
+mongoose.connect(process.env.DB_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }).then(()=>{
+    console.log(`connection to database established`)});
+
+const ticketBookingInfosService_router = require('./app/api/index.js');
+
+app.use('/ticket',ticketBookingInfosService_router);
+
+
+
+
+
+// localhost:3003
+app.listen(3004, () => {
+    console.log(" Ticket booking service is up and listening on 3004...")
+});
